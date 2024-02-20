51ce3372fe1b5da2c030f15cb1c4a55a484a133f
Nitish <student.nitishanand@gmail.com>
2024-02-20 17:31:43 +0530

    Add create, update and destroy feature for employees

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,9 +1,47 @@
 class EmployeesController < ApplicationController
+  before_action :set_employee, only: %i[show update destroy]
+
   def index
-    employees = params[:q].present? ? Employee.search(params[:q]) : Employee.all
-    render json: employees, status: 200
+    render json: (search_params[:q].present? ? Employee.search(search_params[:q]) : Employee.all), status: 200
   end
 
   def show
+    render json: @emp, status: 200
+  end
+
+  def create
+    emp = Employee.new(emp_params)
+    if emp.valid?
+      emp.save!
+    else
+      render json: 'Employee could not be created', status: 500
+    end
+  end
+
+  def update
+    if @emp.present?
+      @emp.update(emp_params)
+    else
+      render json: 'Employee could not be update', status: 500
+    end
+  end
+
+  def destroy
+    @emp.destroy if @emp.present?
+  end
+
+
+  private
+
+  def emp_params
+    params.require(:employee).permit(:ename, :job, :mgr_id, :hireddate, :sal, :comm, :department_id)
+  end
+
+  def search_params
+    params.permit(:q)
+  end
+
+  def set_employee
+    @emp = Employee.find(params[:id])
   end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,6 +1,8 @@
 class Employee < ApplicationRecord
   include Searchable
 
+  validates :ename, :job, :sal, :comm, :hireddate, presence: true
+
   has_many :subordinates, class_name: 'Employee',
     foreign_key: :mgr_id
   belongs_to :mgr, class_name: 'Employee', optional: true
